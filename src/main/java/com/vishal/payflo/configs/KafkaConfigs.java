cefa0dd47bf9c1a2d1ca9d2b439d0a8eb07f573a
Vishal Sharma <vishalimp03@gmail.com>
2026-07-08 22:29:02 +0530

    refacto: correct imports for StringSerializer and StringDesrialiser in KafkaConfigs.java

--- src/main/java/com/vishal/payflo/configs/KafkaConfigs.java
+++ src/main/java/com/vishal/payflo/configs/KafkaConfigs.java
@@ -3,6 +3,8 @@ package com.vishal.payflo.configs;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +12,6 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
 import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
-import tools.jackson.databind.deser.jdk.StringDeserializer;
-import tools.jackson.databind.ser.jdk.StringSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
