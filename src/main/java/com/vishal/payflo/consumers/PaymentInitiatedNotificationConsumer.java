072854aa8e58569e5d58cfbbcc42f83a09aadad4
Vishal Sharma <vishalimp03@gmail.com>
2026-07-11 22:09:19 +0530

    feat: add PaymentInitiatedNotificationConsumer and PaymentInitiatedNotificationEvent for handling payment initiation notifications

---
+++ src/main/java/com/vishal/payflo/consumers/PaymentInitiatedNotificationConsumer.java
@@ -0,0 +1,21 @@
+package com.vishal.payflo.consumers;
+
+import com.vishal.payflo.events.PaymentInitiatedNotificationEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class PaymentInitiatedNotificationConsumer {
+
+
+    @KafkaListener(topics="payflo.notification.payment-initiated", groupId = "payflo-consumer-group")
+    public void sendPaymentTransactionInitiatedNotification(PaymentInitiatedNotificationEvent paymentInitiatedNotificationEvent){
+//        Notification Simulation via logging
+        log.info(paymentInitiatedNotificationEvent.payload());
+
+        log.info("Notification sent for transactionId:{}, event:{}", paymentInitiatedNotificationEvent.transactionId(), "Payment-Initiated-Notification");
+    }
+
+}
