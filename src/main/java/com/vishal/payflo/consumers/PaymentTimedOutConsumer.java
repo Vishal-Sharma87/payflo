ea0c3d0b4fdb3f56bc83237151c5e8149a272b0d
Vishal Sharma <vishalimp03@gmail.com>
2026-07-11 22:10:46 +0530

    feat: add PaymentTimedOutConsumer and PaymentTimedOutEvent for handling payment timed-out notifications

---
+++ src/main/java/com/vishal/payflo/consumers/PaymentTimedOutConsumer.java
@@ -0,0 +1,33 @@
+package com.vishal.payflo.consumers;
+
+import com.vishal.payflo.events.PaymentFailedEvent;
+import com.vishal.payflo.events.PaymentReceivedEvent;
+import com.vishal.payflo.events.PaymentTimedOutEvent;
+import com.vishal.payflo.services.PaymentTransactionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class PaymentTimedOutConsumer {
+
+
+    private final PaymentTransactionService paymentTransactionService;
+
+    public PaymentTimedOutConsumer(PaymentTransactionService paymentTransactionService){
+        this.paymentTransactionService = paymentTransactionService;
+    }
+
+
+    @KafkaListener(topics = "payflo.payment-timed-out", groupId = "payflo-consumer.group")
+    public void consumePaymentTimedOutEvent(PaymentTimedOutEvent paymentTimedOutEvent){
+        paymentTransactionService.markTransactionStatusTimedOut(paymentTimedOutEvent.transactionId());
+
+//        TODO SIMULATE WITH ACTUAL REDIS
+        log.info("REDIS simulation of payment timed-out for transactionId:{} is successful", paymentTimedOutEvent.transactionId());
+
+//        TODO fire the termination notification (payment-timed-out) event
+        log.info("Termination Notification for transactionId:{}, event:{} is successful", paymentTimedOutEvent.transactionId(), "Payment-TimedOut");
+    }
+}
