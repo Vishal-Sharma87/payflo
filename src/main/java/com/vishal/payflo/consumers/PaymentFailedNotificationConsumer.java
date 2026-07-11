1f540fcc75cb3689f7b8c00c6eccffc71ba579e5
Vishal Sharma <vishalimp03@gmail.com>
2026-07-11 22:10:09 +0530

    feat: add PaymentFailedNotificationConsumer and PaymentFailedNotificationEvent for handling payment failure notifications

---
+++ src/main/java/com/vishal/payflo/consumers/PaymentFailedNotificationConsumer.java
@@ -0,0 +1,19 @@
+package com.vishal.payflo.consumers;
+
+import com.vishal.payflo.events.PaymentFailedNotificationEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class PaymentFailedNotificationConsumer {
+
+    @KafkaListener(topics = "payflo.notification.payment-failed", groupId = "payflo-consumer-group")
+    public void sendPaymentTransactionFailedNotification(PaymentFailedNotificationEvent paymentFailedNotificationEvent){
+        log.info(paymentFailedNotificationEvent.payload());
+
+        log.info("Notification sent for transactionId:{}, event:{}", paymentFailedNotificationEvent.transactionId(), "Payment-Failed-Notification");
+    }
+
+}
