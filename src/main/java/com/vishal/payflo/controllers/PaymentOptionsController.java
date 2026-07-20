136e90d63b909319bd8ebeb0d6e37b8eb9decc56
Vishal Sharma <vishalimp03@gmail.com>
2026-07-20 16:55:31 +0530

    refactor: remove unused imports from KafkaConfigs and PaymentInitiateResponseDto

--- src/main/java/com/vishal/payflo/controllers/PaymentOptionsController.java
+++ src/main/java/com/vishal/payflo/controllers/PaymentOptionsController.java
@@ -1,17 +1,20 @@
 package com.vishal.payflo.controllers;
 
 import com.vishal.payflo.dtos.ApiResponse;
+import com.vishal.payflo.dtos.responsedtos.PaymentOptionsResponse;
 import com.vishal.payflo.enums.PaymentType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class PaymentOptionsController {
 
     @GetMapping("/payment/options")
-    public ResponseEntity<ApiResponse<PaymentType[]>> getAvailablePaymentOptions(){
-        return ResponseEntity.ok(ApiResponse.of(PaymentType.values()));
+    public ResponseEntity<ApiResponse<PaymentOptionsResponse>> getAvailablePaymentOptions(){
+        return ResponseEntity.ok(ApiResponse.of(new PaymentOptionsResponse(List.of(PaymentType.values()))));
     }
 
 }
