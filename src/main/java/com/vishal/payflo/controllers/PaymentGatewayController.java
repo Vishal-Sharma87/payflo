664e55683bc7e1480277fa7b07b8cbf333a14830
Vishal Sharma <vishalimp03@gmail.com>
2026-07-20 16:37:09 +0530

    feat: add PaymentGatewayController and PaymentGatewayService for payment transaction processing

---
+++ src/main/java/com/vishal/payflo/controllers/PaymentGatewayController.java
@@ -0,0 +1,27 @@
+package com.vishal.payflo.controllers;
+
+import com.vishal.payflo.dtos.requestdtos.PaymentConfirmRequestDto;
+import com.vishal.payflo.services.PaymentGatewayService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PaymentGatewayController {
+
+
+    private final PaymentGatewayService paymentGatewayService;
+
+    public PaymentGatewayController(PaymentGatewayService paymentGatewayService){
+        this.paymentGatewayService = paymentGatewayService;
+    }
+
+
+    @PostMapping("/payment/confirm")
+    public ResponseEntity<Void> completePaymentTransaction(@RequestBody PaymentConfirmRequestDto paymentConfirmRequestDto){
+        paymentGatewayService.completeTransaction(paymentConfirmRequestDto);
+
+        return ResponseEntity.accepted().build();
+
+    }
+
+}
---
+++ src/main/java/com/vishal/payflo/services/PaymentGatewayService.java
@@ -0,0 +1,11 @@
+package com.vishal.payflo.services;
+
+import com.vishal.payflo.dtos.requestdtos.PaymentConfirmRequestDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentGatewayService {
+    public void completeTransaction(PaymentConfirmRequestDto paymentConfirmRequestDto) {
+
+    }
+}
