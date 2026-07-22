d70ce54521145f61c84e1a3db383ff75f9170129
Vishal Sharma <vishalimp03@gmail.com>
2026-07-23 02:12:32 +0530

    feat: add new exception messages for VPA validation errors

--- src/main/java/com/vishal/payflo/configs/ExceptionMessagesProperties.java
+++ src/main/java/com/vishal/payflo/configs/ExceptionMessagesProperties.java
@@ -4,5 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "payflo.exception.message")
 public record ExceptionMessagesProperties(
-        String transactionNotFound
+        String transactionNotFound,
+        String vpaSeparatorCountMismatch,
+        String vpaInvalidIdentifierFormat,
+        String vpaInvalidPaymentServiceProviderFormat,
+        String vpaUnknownPaymentServiceProvider
 ) {}
