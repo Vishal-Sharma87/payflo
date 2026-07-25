faca709fb2d55c41a77c2ff85e637e8a6af63edf
Vishal Sharma <vishalimp03@gmail.com>
2026-07-26 03:42:48 +0530

    feat: integrate RedisHashService and RedisZSetService into PaymentFailedConsumer for transaction failure handling

--- src/main/java/com/vishal/payflo/consumers/PaymentFailedConsumer.java
+++ src/main/java/com/vishal/payflo/consumers/PaymentFailedConsumer.java
@@ -1,9 +1,13 @@
 package com.vishal.payflo.consumers;
 
+import com.vishal.payflo.cache.service.RedisHashService;
+import com.vishal.payflo.cache.service.RedisZSetService;
+import com.vishal.payflo.enums.TransactionStatus;
 import com.vishal.payflo.kafka.EventPublisher;
 import com.vishal.payflo.kafka.events.PaymentEvent;
 import com.vishal.payflo.kafka.events.PaymentFailedEvent;
 import com.vishal.payflo.kafka.events.PaymentFailedNotificationEvent;
+import com.vishal.payflo.kafka.topics.KafkaTopic;
 import com.vishal.payflo.notifications.NotificationMessageTemplateBuilder;
 import com.vishal.payflo.services.PaymentTransactionService;
 import lombok.extern.slf4j.Slf4j;
@@ -19,31 +23,37 @@ public class PaymentFailedConsumer {
     public final PaymentTransactionService paymentTransactionService;
     private final NotificationMessageTemplateBuilder notificationMessageTemplateBuilder;
     private final EventPublisher eventPublisher;
+    private final RedisHashService redisHashService;
+    private final RedisZSetService redisZSetService;
 
 
 
     public PaymentFailedConsumer(PaymentTransactionService paymentTransactionService,
                                  NotificationMessageTemplateBuilder notificationMessageTemplateBuilder,
-                                 EventPublisher eventPublisher){
+                                 EventPublisher eventPublisher,
+                                 RedisHashService redisHashService,
+                                 RedisZSetService redisZSetService){
         this.paymentTransactionService = paymentTransactionService;
         this.notificationMessageTemplateBuilder = notificationMessageTemplateBuilder;
         this.eventPublisher = eventPublisher;
+        this.redisHashService = redisHashService;
+        this.redisZSetService = redisZSetService;
     }
 
 
     @KafkaListener(topics = "payflo.payment-failed", groupId="payflo-consumer-group")
     public void consumePaymentFailedEvent(PaymentFailedEvent paymentFailedEvent){
         UUID transactionId = paymentFailedEvent.transactionId();
+        KafkaTopic topic = paymentFailedEvent.topic();
 
         paymentTransactionService.markPaymentTransactionFailed(transactionId);
 
-        log.info("Redis simulation for transactionId:{} event:{}", transactionId, "Payment-Failed");
+        redisHashService.finalizeStatus(transactionId, TransactionStatus.FAILED);
+        redisZSetService.remove(transactionId);
 
-        String message = notificationMessageTemplateBuilder.build(paymentFailedEvent.topic(), transactionId);
+        String message = notificationMessageTemplateBuilder.build(topic, transactionId);
         PaymentEvent paymentEvent = new PaymentFailedNotificationEvent(transactionId, message);
         eventPublisher.publish(paymentEvent);
-
-        log.info("Termination notification for transactionId:{}, event:{}", transactionId, "Payment-Failed");
     }
 
 }
