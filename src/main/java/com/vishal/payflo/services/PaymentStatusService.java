c49828a510168e81d040d4b35b789fbf52023880
Vishal Sharma <vishalimp03@gmail.com>
2026-07-26 03:40:38 +0530

    feat: refactor PaymentStatusService to use Redis for transaction status retrieval and the only source of truth per payment transaction

--- src/main/java/com/vishal/payflo/services/PaymentStatusService.java
+++ src/main/java/com/vishal/payflo/services/PaymentStatusService.java
@@ -1,6 +1,7 @@
 package com.vishal.payflo.services;
 
 import com.vishal.payflo.advice.exceptions.PaymentTransactionNotFoundException;
+import com.vishal.payflo.cache.service.RedisHashService;
 import com.vishal.payflo.configs.ExceptionMessagesProperties;
 import com.vishal.payflo.configs.PaymentStatusMessagesProperties;
 import com.vishal.payflo.dtos.responsedtos.PaymentStatusResponseDto;
@@ -13,30 +14,33 @@ import java.util.UUID;
 @Service
 public class PaymentStatusService {
 
-    private final PaymentTransactionService paymentTransactionService;
     private final PaymentStatusMessagesProperties statusMessagesProperties;
     private final ExceptionMessagesProperties exceptionMessagesProperties;
+    private final RedisHashService redisHashService;
 
-    public PaymentStatusService(PaymentTransactionService paymentTransactionService,
-                                PaymentStatusMessagesProperties statusMessagesProperties,
-                                ExceptionMessagesProperties exceptionMessagesProperties){
-        this.paymentTransactionService = paymentTransactionService;
+    public PaymentStatusService(PaymentStatusMessagesProperties statusMessagesProperties,
+                                ExceptionMessagesProperties exceptionMessagesProperties,
+                                RedisHashService redisHashService){
+
         this.statusMessagesProperties = statusMessagesProperties;
         this.exceptionMessagesProperties = exceptionMessagesProperties;
+        this.redisHashService = redisHashService;
     }
 
     public PaymentStatusResponseDto check(UUID transactionId){
+        String hashedStatus = redisHashService.getStatus(transactionId);
+        if (hashedStatus == null)
+            throw new PaymentTransactionNotFoundException(exceptionMessagesProperties.transactionNotFound());
 
-        TransactionStatus status = paymentTransactionService.findTransactionStatusById(transactionId)
-                .orElseThrow(() -> new PaymentTransactionNotFoundException(exceptionMessagesProperties.transactionNotFound()));
+        TransactionStatus transactionStatus= TransactionStatus.valueOf(hashedStatus);
 
-        String message = switch (status){
+        String message = switch (transactionStatus){
             case PROCESSING -> statusMessagesProperties.processing();
             case FAILED -> statusMessagesProperties.failed();
             case TIMED_OUT -> statusMessagesProperties.timedOut();
             case COMPLETED -> statusMessagesProperties.completed();
         };
 
-        return new PaymentStatusResponseDto(transactionId, status, message);
+        return new PaymentStatusResponseDto(transactionId, transactionStatus, message);
     }
 }
