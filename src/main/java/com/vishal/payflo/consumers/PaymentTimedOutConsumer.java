d421e2b8183e3ad657bbe5c5f65c4e736bcf21e0
Vishal Sharma <vishalimp03@gmail.com>
2026-07-26 03:40:49 +0530

    feat: integrate RedisHashService and RedisZSetService into PaymentTimedOutConsumer for enhanced transaction status management

--- src/main/java/com/vishal/payflo/consumers/PaymentTimedOutConsumer.java
+++ src/main/java/com/vishal/payflo/consumers/PaymentTimedOutConsumer.java
@@ -1,9 +1,13 @@
 package com.vishal.payflo.consumers;
 
+import com.vishal.payflo.cache.service.RedisHashService;
+import com.vishal.payflo.cache.service.RedisZSetService;
+import com.vishal.payflo.enums.TransactionStatus;
 import com.vishal.payflo.kafka.EventPublisher;
 import com.vishal.payflo.kafka.events.PaymentEvent;
 import com.vishal.payflo.kafka.events.PaymentTimedOutEvent;
 import com.vishal.payflo.kafka.events.PaymentTimedOutNotificationEvent;
+import com.vishal.payflo.kafka.topics.KafkaTopic;
 import com.vishal.payflo.notifications.NotificationMessageTemplateBuilder;
 import com.vishal.payflo.services.PaymentTransactionService;
 import lombok.extern.slf4j.Slf4j;
@@ -20,28 +24,36 @@ public class PaymentTimedOutConsumer {
     private final PaymentTransactionService paymentTransactionService;
     private final NotificationMessageTemplateBuilder notificationMessageTemplateBuilder;
     private final EventPublisher eventPublisher;
+    private final RedisHashService redisHashService;
+    private final RedisZSetService redisZSetService;
+
 
     public PaymentTimedOutConsumer(PaymentTransactionService paymentTransactionService,
                                    NotificationMessageTemplateBuilder notificationMessageTemplateBuilder,
-                                   EventPublisher eventPublisher){
+                                   EventPublisher eventPublisher,
+                                   RedisHashService redisHashService,
+                                   RedisZSetService redisZSetService){
         this.paymentTransactionService = paymentTransactionService;
         this.notificationMessageTemplateBuilder = notificationMessageTemplateBuilder;
         this.eventPublisher = eventPublisher;
+        this.redisHashService  = redisHashService;
+        this.redisZSetService = redisZSetService;
     }
 
 
     @KafkaListener(topics = "payflo.payment-timed-out", groupId = "payflo-consumer.group")
     public void consumePaymentTimedOutEvent(PaymentTimedOutEvent paymentTimedOutEvent){
         UUID transactionId = paymentTimedOutEvent.transactionId();
+        KafkaTopic topic = paymentTimedOutEvent.topic();
 
         paymentTransactionService.markTransactionStatusTimedOut(transactionId);
 
-        log.info("REDIS simulation of payment timed-out for transactionId:{} is successful", transactionId);
+        redisHashService.finalizeStatus(transactionId, TransactionStatus.TIMED_OUT);
+        redisZSetService.remove(transactionId);
 
-        String message = notificationMessageTemplateBuilder.build(paymentTimedOutEvent.topic(), transactionId);
+        String message = notificationMessageTemplateBuilder.build(topic, transactionId);
         PaymentEvent paymentEvent = new PaymentTimedOutNotificationEvent(transactionId, message);
         eventPublisher.publish(paymentEvent);
 
-        log.info("Termination Notification for transactionId:{}, event:{} is successful", transactionId, "Payment-TimedOut");
     }
 }
