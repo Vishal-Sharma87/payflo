b0b7a0fc44faff65618f2001ff9fdad55aff1d99
Vishal Sharma <vishalimp03@gmail.com>
2026-07-26 03:41:58 +0530

    feat: integrate and perform actual Redis components in PaymentReceivedConsumer

--- src/main/java/com/vishal/payflo/consumers/PaymentReceivedConsumer.java
+++ src/main/java/com/vishal/payflo/consumers/PaymentReceivedConsumer.java
@@ -1,9 +1,13 @@
 package com.vishal.payflo.consumers;
 
+import com.vishal.payflo.cache.service.RedisHashService;
+import com.vishal.payflo.cache.service.RedisZSetService;
+import com.vishal.payflo.enums.TransactionStatus;
 import com.vishal.payflo.kafka.EventPublisher;
 import com.vishal.payflo.kafka.events.PaymentCompletedNotificationEvent;
 import com.vishal.payflo.kafka.events.PaymentEvent;
 import com.vishal.payflo.kafka.events.PaymentReceivedEvent;
+import com.vishal.payflo.kafka.topics.KafkaTopic;
 import com.vishal.payflo.notifications.NotificationMessageTemplateBuilder;
 import com.vishal.payflo.services.PaymentTransactionService;
 import lombok.extern.slf4j.Slf4j;
@@ -19,29 +23,35 @@ public class PaymentReceivedConsumer {
     private final PaymentTransactionService paymentTransactionService;
     private final NotificationMessageTemplateBuilder notificationMessageTemplateBuilder;
     private final EventPublisher eventPublisher;
+    private final RedisHashService redisHashService;
+    private final RedisZSetService redisZSetService;
 
     public PaymentReceivedConsumer(PaymentTransactionService paymentTransactionService,
                                    NotificationMessageTemplateBuilder notificationMessageTemplateBuilder,
-                                   EventPublisher eventPublisher){
+                                   EventPublisher eventPublisher,
+                                   RedisHashService redisHashService,
+                                   RedisZSetService redisZSetService){
         this.paymentTransactionService = paymentTransactionService;
         this.notificationMessageTemplateBuilder = notificationMessageTemplateBuilder;
         this.eventPublisher = eventPublisher;
+        this.redisHashService = redisHashService;
+        this.redisZSetService = redisZSetService;
     }
 
 
     @KafkaListener(topics = "payflo.payment-received", groupId = "payflo-consumer.group")
-    public void completeTransaction(PaymentReceivedEvent paymentReceivedEvent){
+    public void completeTransaction(PaymentReceivedEvent paymentReceivedEvent) {
         UUID transactionId = paymentReceivedEvent.transactionId();
+        KafkaTopic topic = paymentReceivedEvent.topic();
 
         paymentTransactionService.markTransactionStatusCompleted(transactionId);
 
-        log.info("REDIS simulation of payment received for transactionId:{} is successful", transactionId);
+        redisHashService.finalizeStatus(transactionId, TransactionStatus.COMPLETED);
+        redisZSetService.remove(transactionId);
 
-        String message = notificationMessageTemplateBuilder.build(paymentReceivedEvent.topic(), transactionId);
+        String message = notificationMessageTemplateBuilder.build(topic, transactionId);
         PaymentEvent paymentEvent = new PaymentCompletedNotificationEvent(transactionId, message);
         eventPublisher.publish(paymentEvent);
-
-        log.info("Termination Notification (payment-completed) for transactionId:{} is successful", transactionId);
     }
 
 }
