602205f5825e8e41f332a0ba7b2f218b0480ded3
Vishal Sharma <vishalimp03@gmail.com>
2026-07-28 03:56:01 +0530

    feat: integrate NotificationPublisher in PaymentInitiatedNotificationConsumer for sending payment initiated notifications

--- src/main/java/com/vishal/payflo/consumers/PaymentInitiatedNotificationConsumer.java
+++ src/main/java/com/vishal/payflo/consumers/PaymentInitiatedNotificationConsumer.java
@@ -1,20 +1,31 @@
 package com.vishal.payflo.consumers;
 
 import com.vishal.payflo.kafka.events.PaymentInitiatedNotificationEvent;
+import com.vishal.payflo.kafka.topics.KafkaTopic;
+import com.vishal.payflo.notifications.NotificationPublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @Slf4j
 public class PaymentInitiatedNotificationConsumer {
 
+    private final NotificationPublisher notificationPublisher;
+    public PaymentInitiatedNotificationConsumer(NotificationPublisher notificationPublisher){
+        this.notificationPublisher = notificationPublisher;
+    }
+
 
     @KafkaListener(topics="payflo.notification.payment-initiated", groupId = "payflo-consumer-group")
     public void sendPaymentTransactionInitiatedNotification(PaymentInitiatedNotificationEvent paymentInitiatedNotificationEvent){
-        log.info(paymentInitiatedNotificationEvent.payload());
+        UUID transactionId = paymentInitiatedNotificationEvent.transactionId();
+        KafkaTopic kafkaTopic = paymentInitiatedNotificationEvent.topic();
+        String payload = paymentInitiatedNotificationEvent.payload();
 
-        log.info("Notification sent for transactionId:{}, event:{}", paymentInitiatedNotificationEvent.transactionId(), paymentInitiatedNotificationEvent.topic());
+        notificationPublisher.publish(transactionId, payload, kafkaTopic);
     }
 
 }
