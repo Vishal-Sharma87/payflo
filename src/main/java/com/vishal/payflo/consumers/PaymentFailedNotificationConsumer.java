88778383ebc5eae45bd88d72ddb045f4e7fd053e
Vishal Sharma <vishalimp03@gmail.com>
2026-07-28 03:57:11 +0530

    feat: replace manual atomic operation checks with centralized Redis execution for notification event using NotificationPublisher

--- src/main/java/com/vishal/payflo/consumers/PaymentFailedNotificationConsumer.java
+++ src/main/java/com/vishal/payflo/consumers/PaymentFailedNotificationConsumer.java
@@ -1,19 +1,34 @@
 package com.vishal.payflo.consumers;
 
+import com.vishal.payflo.cache.service.RedisHashService;
 import com.vishal.payflo.kafka.events.PaymentFailedNotificationEvent;
+import com.vishal.payflo.kafka.topics.KafkaTopic;
+import com.vishal.payflo.notifications.NotificationPublisher;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @Slf4j
 public class PaymentFailedNotificationConsumer {
 
+
+    private final NotificationPublisher notificationPublisher;
+    public PaymentFailedNotificationConsumer(NotificationPublisher notificationPublisher){
+        this.notificationPublisher = notificationPublisher;
+    }
+
+
     @KafkaListener(topics = "payflo.notification.payment-failed", groupId = "payflo-consumer-group")
     public void sendPaymentTransactionFailedNotification(PaymentFailedNotificationEvent paymentFailedNotificationEvent){
-        log.info(paymentFailedNotificationEvent.payload());
+        UUID transactionId = paymentFailedNotificationEvent.transactionId();
+        KafkaTopic kafkaTopic = paymentFailedNotificationEvent.topic();
+        String payload = paymentFailedNotificationEvent.payload();
 
-        log.info("Notification sent for transactionId:{}, event:{}", paymentFailedNotificationEvent.transactionId(), paymentFailedNotificationEvent.topic());
+        notificationPublisher.publish(transactionId, payload, kafkaTopic);
+
     }
 
 }
