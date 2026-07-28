4a8c5b00757881d68d319fbce420e70a99aa0409
Vishal Sharma <vishalimp03@gmail.com>
2026-07-29 00:56:31 +0530

    refactor: [Logging Phase 4.4] [Services] enhance PaymentTransactionService with logging for transaction status updates

--- src/main/java/com/vishal/payflo/services/PaymentTransactionService.java
+++ src/main/java/com/vishal/payflo/services/PaymentTransactionService.java
@@ -5,11 +5,13 @@ import com.vishal.payflo.repositories.PaymentTransactionRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class PaymentTransactionService {
 
     @PersistenceContext
@@ -24,21 +26,29 @@ public class PaymentTransactionService {
 
     @Transactional
     public void markTransactionStatusCompleted(UUID transactionId) {
+        log.info("Marking transaction as COMPLETED for transactionId:{}", transactionId);
         paymentTransactionRepository.markTransactionStatusCompleted(transactionId);
+        log.info("Marked transaction as COMPLETED for transactionId:{}", transactionId);
     }
 
     @Transactional
     public void markPaymentTransactionFailed(UUID transactionId) {
+        log.info("Marking transaction as FAILED for transactionId:{}", transactionId);
         paymentTransactionRepository.markTransactionStatusFailed(transactionId);
+        log.info("Marked transaction as FAILED for transactionId:{}", transactionId);
     }
 
     @Transactional
     public void markTransactionStatusTimedOut(UUID transactionId) {
+        log.info("Marking transaction as TIMED_OUT for transactionId:{}", transactionId);
         paymentTransactionRepository.markTransactionStatusTimedOut(transactionId);
+        log.info("Marked transaction as TIMED_OUT for transactionId:{}", transactionId);
     }
 
     @Transactional
     public void persistNewTransaction(PaymentTransaction paymentTransaction) {
+        log.info("Persisting new transaction for transactionId:{}", paymentTransaction.getTransactionId());
         entityManager.persist(paymentTransaction);
+        log.info("Persisted new transaction for transactionId:{}", paymentTransaction.getTransactionId());
     }
 }
