111e5100bb9cf73cb679a07709870bcb16d919b5
Vishal Sharma <vishalimp03@gmail.com>
2026-07-29 00:58:06 +0530

    refactor: add logging for received notification events

--- src/main/java/com/vishal/payflo/consumers/PaymentTimedOutNotificationConsumer.java
+++ src/main/java/com/vishal/payflo/consumers/PaymentTimedOutNotificationConsumer.java
@@ -26,6 +26,7 @@ public class PaymentTimedOutNotificationConsumer {
         KafkaTopic kafkaTopic = paymentTimedOutNotificationEvent.topic();
         String payload = paymentTimedOutNotificationEvent.payload();
 
+        log.info("Received notification event for transactionId:{} topic:{}", transactionId, kafkaTopic);
         notificationPublisher.publish(transactionId, payload, kafkaTopic);
     }
 
