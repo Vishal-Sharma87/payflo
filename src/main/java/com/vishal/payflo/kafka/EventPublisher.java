01e61cd82fddf683b379e75588c5c94fb08c7077
Vishal Sharma <vishalimp03@gmail.com>
2026-07-29 00:57:47 +0530

    refactor: [Logging Phase 4.4] [EventPublisher] add logging for event publishing process

--- src/main/java/com/vishal/payflo/kafka/EventPublisher.java
+++ src/main/java/com/vishal/payflo/kafka/EventPublisher.java
@@ -2,10 +2,12 @@ package com.vishal.payflo.kafka;
 
 import com.vishal.payflo.kafka.events.PaymentEvent;
 import com.vishal.payflo.kafka.topics.KafkaTopicResolver;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EventPublisher {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
@@ -21,7 +23,9 @@ public class EventPublisher {
         String key = paymentEvent.key();
         String topicName = kafkaTopicResolver.resolve(paymentEvent.topic());
 
+        log.info("Publishing event for transactionId:{} topic:{}", key, paymentEvent.topic());
         kafkaTemplate.send(topicName, key, paymentEvent);
+        log.info("Published event for transactionId:{} topic:{}", key, paymentEvent.topic());
     }
 
 }
