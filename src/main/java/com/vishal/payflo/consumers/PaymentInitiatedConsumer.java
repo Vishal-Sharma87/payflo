58968d27448602e258ec2697898cd04b135645e0
Vishal Sharma <vishalimp03@gmail.com>
2026-07-28 20:16:20 +0530

    refactor: update the flow of initializing a new PaymentTransaction, now using Lua for atomic redis operations

--- src/main/java/com/vishal/payflo/consumers/PaymentInitiatedConsumer.java
+++ src/main/java/com/vishal/payflo/consumers/PaymentInitiatedConsumer.java
@@ -1,20 +1,19 @@
 package com.vishal.payflo.consumers;
 
-import com.vishal.payflo.cache.service.RedisHashService;
-import com.vishal.payflo.cache.service.RedisZSetService;
+import com.vishal.payflo.cache.service.TransactionInitializationService;
 import com.vishal.payflo.entities.PaymentTransaction;
 import com.vishal.payflo.kafka.EventPublisher;
 import com.vishal.payflo.kafka.events.PaymentEvent;
 import com.vishal.payflo.kafka.events.PaymentInitiatedEvent;
 import com.vishal.payflo.kafka.events.PaymentInitiatedNotificationEvent;
+import com.vishal.payflo.kafka.topics.KafkaTopic;
 import com.vishal.payflo.notifications.NotificationMessageTemplateBuilder;
 import com.vishal.payflo.services.PaymentTransactionService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
 import java.util.UUID;
 
 @Service
@@ -24,40 +23,37 @@ public class PaymentInitiatedConsumer {
     private final PaymentTransactionService paymentTransactionService;
     private final NotificationMessageTemplateBuilder notificationMessageTemplateBuilder;
     private final EventPublisher eventPublisher;
-    private final RedisHashService redisHashService;
-    private final RedisZSetService redisZSetService;
+    private final TransactionInitializationService transactionInitializationService;
 
     public PaymentInitiatedConsumer(PaymentTransactionService paymentTransactionService,
                                     NotificationMessageTemplateBuilder notificationMessageTemplateBuilder,
                                     EventPublisher eventPublisher,
-                                    RedisZSetService redisZSetService,
-                                    RedisHashService redisHashService){
+                                    TransactionInitializationService transactionInitializationService){
         this.paymentTransactionService = paymentTransactionService;
         this.notificationMessageTemplateBuilder = notificationMessageTemplateBuilder;
         this.eventPublisher = eventPublisher;
-        this.redisHashService = redisHashService;
-        this.redisZSetService = redisZSetService;
+        this.transactionInitializationService = transactionInitializationService;
     }
 
-
-    @KafkaListener(topics = "payflo.payment-initiated", groupId = "payflo-consumer-group")
-    public void initiatePayment(PaymentInitiatedEvent paymentInitiatedEvent) {
-        UUID transactionId = paymentInitiatedEvent.transactionId();
-        Instant startedAt = paymentInitiatedEvent.startedAt();
-
+    @KafkaListener(topics = "payflo.payment-initiated", groupId = "payflo-consumer.group")
+    public void initiateTransaction(PaymentInitiatedEvent paymentInitiatedEvent) {
         try {
             PaymentTransaction paymentTransaction = PaymentTransaction.from(paymentInitiatedEvent);
-            paymentTransactionService.createNewTransaction(paymentTransaction);
-
-            redisHashService.createStatus(transactionId);
-            redisZSetService.createEntry(transactionId, startedAt);
-
-            String message = notificationMessageTemplateBuilder.build(paymentInitiatedEvent.topic(), transactionId);
-            PaymentEvent paymentEvent = new PaymentInitiatedNotificationEvent(transactionId, message);
-            eventPublisher.publish(paymentEvent);
-        } catch (DataIntegrityViolationException e) {
-            log.warn("PaymentTransaction with transactionId: {} already exists.", transactionId);
+            paymentTransactionService.persistNewTransaction(paymentTransaction);
+            finalizeInitiation(paymentInitiatedEvent);
+        } catch (DuplicateKeyException e) {
+            finalizeInitiation(paymentInitiatedEvent);
         }
+    }
 
+    private void finalizeInitiation(PaymentInitiatedEvent paymentInitiatedEvent) {
+        UUID transactionId = paymentInitiatedEvent.transactionId();
+        KafkaTopic kafkaTopic = paymentInitiatedEvent.topic();
+
+        transactionInitializationService.initialize(transactionId, paymentInitiatedEvent.startedAt());
+
+        String message = notificationMessageTemplateBuilder.build(kafkaTopic, transactionId);
+        PaymentEvent notificationEvent = new PaymentInitiatedNotificationEvent(transactionId, message);
+        eventPublisher.publish(notificationEvent);
     }
 }
