cfd5f1e837d84d27858b4030d8e60b614db3c6cb
Vishal Sharma <vishalimp03@gmail.com>
2026-08-04 16:53:57 +0530

    feat: refactor NotificationHashKeyResolverTest methods to static for better test organization

--- src/test/java/com/vishal/payflo/notification/NotificationHashKeyResolverTest.java
+++ src/test/java/com/vishal/payflo/notification/NotificationHashKeyResolverTest.java
@@ -4,13 +4,11 @@ import com.vishal.payflo.configs.NotificationHashKeyProperties;
 import com.vishal.payflo.kafka.topics.KafkaTopic;
 import com.vishal.payflo.notifications.NotificationHashKeyResolver;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
 import java.util.stream.Stream;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NotificationHashKeyResolverTest {
     private static final String paymentInitiatedNotification = "notification:payment-initiated";
     private static final String paymentCompletedNotification = "notification:payment-completed";
@@ -33,7 +31,7 @@ public class NotificationHashKeyResolverTest {
         Assertions.assertEquals(expectedHashKey, actual);
     }
 
-    public Stream<Arguments> validTopicsToHashKeys() {
+    public static Stream<Arguments> validTopicsToHashKeys() {
         return Stream.of(
                 Arguments.of(KafkaTopic.PAYMENT_INITIATED_NOTIFICATION, paymentInitiatedNotification),
                 Arguments.of(KafkaTopic.PAYMENT_COMPLETED_NOTIFICATION, paymentCompletedNotification),
@@ -48,7 +46,7 @@ public class NotificationHashKeyResolverTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> notificationHashKeyResolver.resolve(topic));
     }
 
-    public Stream<Arguments> invalidTopicsThrowsException() {
+    public static Stream<Arguments> invalidTopicsThrowsException() {
         return Stream.of(
                 Arguments.of(KafkaTopic.PAYMENT_INITIATED),
                 Arguments.of(KafkaTopic.PAYMENT_RECEIVED),
